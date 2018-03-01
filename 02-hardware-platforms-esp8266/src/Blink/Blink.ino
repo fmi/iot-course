f2f1fc9bbd4fca92aa2e6db0813320eedd0e896c
Pavel Genevski <pavel.genevski@sap.com>
2018-03-01 13:02:56 +0200

    adding blink example

---
+++ 02-hardware-platforms-esp8266/src/Blink/Blink.ino
@@ -0,0 +1,14 @@
+void setup() {
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200L);
+}
+
+void loop() {
+  
+  //Serial.println("Blink");
+  digitalWrite(LED_BUILTIN, LOW);   // LOW = ON on the ESP-01
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(500);
+}
