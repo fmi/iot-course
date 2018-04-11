1cb2efc4ad7132ac6c5a915c7d8efe218ee6dcf4
Genevski <pavel.genevski@sap.com>
2018-04-11 17:03:38 +0300

    improve analog read example

--- 07-device-management/src/AnalogVoltageMeasurement/AnalogVoltageMeasurement.ino
+++ 07-device-management/src/AnalogVoltageMeasurement/AnalogVoltageMeasurement.ino
@@ -2,6 +2,10 @@ unsigned int raw=0;
 float volt=0.0;
 const int sleepTimeS = 60;
 
+const int resistor = 100;
+const float scalingFactor = 5.14; // TODO how was this calculated?
+
+
 void setup() {
 
   Serial.begin(115200);
@@ -12,8 +16,9 @@ void loop() {
 
   pinMode(A0, INPUT);
   raw = analogRead(A0);
+  
   volt=raw/1023.0;
-  volt=volt*4.2;
+  volt=volt*scalingFactor;
 
   Serial.println("Voltage: " + String(volt) + " raw:" + String(raw));
   delay(1000);
