222131527e5e11d8fb6efd6945231f81d4bd8fc9
Trayan Iliev <ilievt@gmail.com>
2018-04-12 12:20:05 +0300

    wifi-manager basic example cleanup

--- 07-device-management/src/wifi-manager/wifi-manager.ino
+++ 07-device-management/src/wifi-manager/wifi-manager.ino
@@ -6,33 +6,6 @@
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 #include <ESP8266mDNS.h>
 
-ESP8266WebServer server(80);
-
-const int led = 13;
-
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
-}
-
-void handleNotFound(){
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
