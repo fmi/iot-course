d7fd5e051cd1a15254776092607d371d8532b0e4
pgenevski <pavel.genevski@gmail.com>
2019-03-28 16:21:10 +0200

    Fixed payload and callbacks

--- 06-connectivity-2/src/01_LoraModuleClient/01_LoraModuleClient.ino
+++ 06-connectivity-2/src/01_LoraModuleClient/01_LoraModuleClient.ino
@@ -1,39 +1,32 @@
-// Original example by https://github.com/matthijskooijman/arduino-lmic/blob/master/examples/ttn-otaa/ttn-otaa.ino
+// A modified exampleof https://github.com/matthijskooijman/arduino-lmic
+
 #include <lmic.h>
 #include <hal/hal.h>
-#include <SPI.h>
 
-static const PROGMEM u1_t NWKSKEY[16] = { 0x85, 0xBB, 0x1B, 0xF9, 0x96, 0xFD, 0xA5, 0xBF, 0xB6, 0x8D, 0x29, 0x8C, 0x88, 0xDD, 0xC1, 0x1E };
+// TTN supplied credentials
+static const PROGMEM u1_t NWKSKEY[16] = { 0x1C, 0x6D, 0x4B, 0x07, 0x2A, 0x99, 0x36, 0x11, 0x61, 0x22, 0xF2, 0x50, 0xA8, 0xDF, 0x99, 0x29 };
+static const PROGMEM u1_t APPSKEY[16] = { 0x8C, 0x14, 0x7A, 0x5D, 0xA6, 0x9D, 0x89, 0xC3, 0x01, 0xF3, 0xE0, 0x7C, 0xE8, 0x3D, 0xDD, 0xE5 };
+static const u4_t DEVADDR = 0x26011E17; 
 
-static const u1_t PROGMEM APPSKEY[16] = { 0xDF, 0x5C, 0xEE, 0x00, 0xB9, 0xEA, 0xBE, 0x56, 0xE9, 0xA3, 0x3C, 0xCC, 0x08, 0xFA, 0x36, 0xF0};
-
-static const u4_t DEVADDR = 0x2601148F; // <-- Change this address for every node!
-
-// These callbacks are only used in over-the-air activation, so they are
-// left empty here (we cannot leave them out completely unless
-// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+// Void callbacks
 void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t mydata[] = "Hello, world!";
+                                   
+//static uint8_t mydata[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09};
+static uint8_t mydata[] = {0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01};
 static osjob_t sendjob;
 
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
-const unsigned TX_INTERVAL = 60;
-
-// Pin mapping
 const lmic_pinmap lmic_pins = {
-    .nss = 8,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = 4,
-    .dio = {0, 1, 2},
+    .nss = 8,                      // Digital pin connected to SS
+    .rxtx = LMIC_UNUSED_PIN,       // we do not use these
+    .rst = 4,                      // Digital pin connected to RST
+    .dio = {7, 1, 2},              // Digital pin connected to 00,01,02 
 };
 
 void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
+  
     switch(ev) {
         case EV_SCAN_TIMEOUT:
             Serial.println(F("EV_SCAN_TIMEOUT"));
@@ -62,17 +55,19 @@ void onEvent (ev_t ev) {
         case EV_REJOIN_FAILED:
             Serial.println(F("EV_REJOIN_FAILED"));
             break;
+            break;
         case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            Serial.println(F("EV_TXCOMPLETE"));
             if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
-            if (LMIC.dataLen) {
-              Serial.println(F("Received "));
-              Serial.println(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
+                Serial.println(F("Received ack"));
+            if(LMIC.dataLen) {
+                // data received in rx slot after tx
+                Serial.print(F("Data Received: "));
+                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+                Serial.println();
             }
-            // Schedule next transmission
-            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            delay(10000);
+            do_send(&sendjob); // data must be prepared in the loop function 
             break;
         case EV_LOST_TSYNC:
             Serial.println(F("EV_LOST_TSYNC"));
@@ -97,61 +92,31 @@ void onEvent (ev_t ev) {
 }
 
 void do_send(osjob_t* j){
-    // Check if there is not a current TX/RX job running
+    
     if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
+        Serial.println(F("OP_TXRXPEND not ready!"));
     } else {
-        // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+        LMIC_setTxData2(1, mydata, sizeof(mydata), 0);
         Serial.println(F("Packet queued"));
     }
-    // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void setup() {
-    delay(2000);  // Small delay to init the USB/Serial on the board. TODO: remove.
-    
+    delay(3000);
     Serial.begin(9600);
     Serial.println(F("Starting"));
-
-    #ifdef VCC_ENABLE
-    // For Pinoccio Scout boards
-    pinMode(VCC_ENABLE, OUTPUT);
-    digitalWrite(VCC_ENABLE, HIGH);
-    delay(1000);
-    #endif
-
-    // LMIC init
+    
     os_init();
-    // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
-
-    // Set static session parameters. Instead of dynamically establishing a session
-    // by joining the network, precomputed session parameters are be provided.
     #ifdef PROGMEM
-    // On AVR, these values are stored in flash and only copied to RAM
-    // once. Copy them to a temporary buffer here, LMIC_setSession will
-    // copy them into a buffer of its own again.
-    uint8_t appskey[sizeof(APPSKEY)];
-    uint8_t nwkskey[sizeof(NWKSKEY)];
-    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
-    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
-    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+      uint8_t appskey[sizeof(APPSKEY)];
+      uint8_t nwkskey[sizeof(NWKSKEY)];
+      memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+      memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+      LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
     #else
-    // If not running an AVR with PROGMEM, just use the arrays directly
-    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+      LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
     #endif
-
-    #if defined(CFG_eu868)
-    // Set up the channels used by the Things Network, which corresponds
-    // to the defaults of most gateways. Without this, only three base
-    // channels from the LoRaWAN specification are used, which certainly
-    // works, so it is good for debugging, but can overload those
-    // frequencies, so be sure to configure the full frequency range of
-    // your network here (unless your network autoconfigures them).
-    // Setting up channels should happen after LMIC_setSession, as that
-    // configures the minimal channel set.
-    // NA-US channels 0-71 are configured automatically
     LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
     LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
     LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
@@ -161,31 +126,19 @@ void setup() {
     LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
     LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
     LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
-    // devices' ping slots. LMIC does not have an easy way to define set this
-    // frequency and support for class B is spotty and untested, so this
-    // frequency is not configured here.
-    #elif defined(CFG_us915)
-    // NA-US channels 0-71 are configured automatically
-    // but only one group of 8 should (a subband) should be active
-    // TTN recommends the second sub band, 1 in a zero based count.
-    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
-    LMIC_selectSubBand(1);
-    #endif
-
-    // Disable link check validation
     LMIC_setLinkCheckMode(0);
-
-    // TTN uses SF9 for its RX2 window.
+    LMIC_setDrTxpow(DR_SF7, 14);    // SF7 and max power from module = 14
+    LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 100);
     LMIC.dn2Dr = DR_SF9;
-
-    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(DR_SF7,14);
-
-    // Start job
     do_send(&sendjob);
 }
 
 void loop() {
-    os_runloop_once();
+
+    os_runloop_once(); //LoRa statemachine
+
+    // here you are supposed to read sensors and update your data
+    //uint8_t tmp = mydata[0];
+    //mydata[0]= mydata[1];
+    //mydata[1]= tmp;
 }
