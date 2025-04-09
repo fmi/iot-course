c9ddd49f8c47f2229f52f8343094a9ee4ad1bc27
pgenevski <pavel.genevski@gmail.com>
2025-04-09 17:16:28 +0300

    docs, reconnect and more info

--- 07-connectivity-2/src/01_WIFI_STA/01_WIFI_STA.ino
+++ 07-connectivity-2/src/01_WIFI_STA/01_WIFI_STA.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+#include <WiFi.h> // https://docs.espressif.com/projects/arduino-esp32/en/latest/api/wifi.html
 
 const char* SSID = "TODO";
 const char* WIFI_PASSWORD = "TODO";
@@ -16,13 +16,44 @@ void setup() {
     Serial.print('.');
     delay(1000);
   }
-  Serial.print("Connected. Local IP: ");
+  Serial.println("Connected");
+
+  Serial.print("ESP32 MAC: ");
+  Serial.println(WiFi.macAddress());
+  
+  Serial.print("Local IP: ");
   Serial.println(WiFi.localIP());
-  // TODO get mac
+
+  Serial.print("Subnet mask: ");
+  Serial.println(WiFi.subnetMask());
+
+  Serial.print("Gateway IP: ");
+  Serial.println(WiFi.gatewayIP());
+  
+  Serial.print("Channel: ");
+  Serial.println(WiFi.channel());
+
+  Serial.print("DNS: ");
+  Serial.println(WiFi.dnsIP());
+  
+  Serial.print("TxPower: ");
+  Serial.println( WiFi.getTxPower());
+
+  IPAddress resolvedIP;
+  WiFi.hostByName("google.com", resolvedIP);
+  Serial.print("Resolved google.com: ");
+  Serial.println(resolvedIP);
 }
 
 void loop(){
 
   Serial.println(WiFi.RSSI()); // -30 to -50 dBm is excellent, -50 to -67 dBm is OK, -67 to -70 dBm shaky
-  delay(1000);
+  delay(2000);
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Reconnecting...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    // alternatively, see WiFi.onEvent
+    // or even simpler, WiFi.setAutoReconnect(true);
+  }
 }
